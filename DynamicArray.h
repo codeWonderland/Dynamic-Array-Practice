2b8f2684b460dfaaa5a2288bd8fff4406b01c4fe
Alice Easter <alice.easter@mymail.champlain.edu>
2017-02-23 23:47:51 -0500

    Commenting shit out the wazoo

--- DynamicArray.h
+++ DynamicArray.h
@@ -22,27 +22,90 @@ private:
 public:
     DynamicArray* mArrayOfArrays;
 
+    /*
+     * Pre: this method requires nothing
+     * Purpose: This method creates a dynamic array
+     *      this array will either hold more
+     *      dynamic arrays inside of it or strings
+     * Post: Creates a dynamic array
+     * * * * * * * * * * * * * * * * * * * * * * * * */
     DynamicArray(int capacity = DEFAULT_INITIAL_CAPACITY, int typeOfArray = 1, std::string name = "Array");
 
-    //Copy Constructor
+    /*
+     * Pre: A dynamic array and a size for a new array
+     * Purpose: This method acts a copy constructor
+     * Post: Creates a new Dynamic Array based off an
+     *      already existing one
+     * * * * * * * * * * * * * * * * * * * * * * * * * */
     DynamicArray(int capacity, const DynamicArray& oldDynamicArray);
 
+    /*
+     * Pre: A dynamic array
+     * Purpose: Add one more open spot in a dynamic array
+     * Post: returns nothing
+     * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void addNewDynamicArray();
 
+    /*
+     * Pre: a dynamic array and a new size for the array
+     * Purpose: change the size of a dynamic array to
+     *      match the defined size
+     * Post: returns nothing
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     void resize(int newCapacity);
 
+    /*
+     * Pre: A dynamic array
+     * Purpose: to return the size of a dynamic array
+     * Post: Returns the size of a dynamic array
+     * * * * * * * * * * * * * * * * * * * * * * * * * */
     int getCapacity() const;
 
+    /*
+     * Pre: a dynamic array
+     * Purpose: Returns a dynamic array
+     * Post: Returns a dynamic array
+     * * * * * * * * * * * * * * * * * */
     std::string *getArray() const;
 
+    /*
+     * Pre: a dynamic array of arrays
+     * Purpose: returns a dynamic array of arrays
+     * Post: returns a dynamic array of arrays
+     * * * * * * * * * * * * * * * * * * * * * * * */
     DynamicArray *getArrayOfArrays() const;
 
+    /*
+     * Pre: a dynamic array
+     * Purpose: returns the name of a dynamic array
+     * Post: returns the name of a dynamic array
+     * * * * * * * * * * * * * * * * * * * * * * * * */
     std::string getName() const;
 
+    /*
+     * Pre: a dynamic array
+     * Purpose: to set a specific value in a
+     *      dynamic array
+     * Post: returns nothing
+     * * * * * * * * * * * * * * * * * * * * */
     void setArrayValue(std::string value, int location);
 
+    /*
+     * Pre: a dynamic array and a string
+     * Purpose: adds a string to a dynamic array
+     * Post: returns nothing
+     * * * * * * * * * * * * * * * * * * * * * * */
     void operator+=(std::string newString);
 
+    /*
+     * Pre: a dynamic array and a string
+     * Purpose: parses through a dynamic array
+     *      looking for a string, if this string
+     *      is found then it is removed and the
+     *      values shifted to the left to remove
+     *      the blank spaces
+     * Post: returns nothing
+     * * * * * * * * * * * * * * * * * * * * * * */
     void operator-=(std::string value);
 
     ~DynamicArray();
--- functions.h
+++ functions.h
@@ -7,6 +7,13 @@
 
 #include "DynamicArray.h"
 
+/*
+ * Pre: a Dynamic Array class
+ * Purpose: Creates a dynamic array to fill with user modifiable
+ *      dynamic arrays, then allows the user to create said modifiable
+ *      arrays, then add to them from files or delete values from them
+ * Post: returns nothing
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void mainMenu();
 
 #endif //PA2_FUNCTIONS_H
