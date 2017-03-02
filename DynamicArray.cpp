fb2d7d1edf1db270155bb727ee097dab7b5fad8c
Alice Easter <alice.easter@mymail.champlain.edu>
2017-03-02 16:09:34 -0500

    Added comments to claim work as mine

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -1,6 +1,21 @@
-//
-// Created by Alice Easter on 2/23/17.
-//
+/*
+Author: Alice Easter
+        Class: CSI 240-05
+        Assignment: PA 2
+Date Assigned: 02/23/17
+        Due Date: 03/07/17 12:29 PM
+Description:
+        Creates a dynamic array that is modifiable by a user
+Certification of Authenticity:
+        I certify that this is entirely my own work, except where I have given
+        fully-documented references to the work of others. I understand the definition and
+consequences of plagiarism and acknowledge that the assessor of this assignment
+        may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for the
+- purpose of future plagiarism checking)
+*/
 
 #include "DynamicArray.h"
 
